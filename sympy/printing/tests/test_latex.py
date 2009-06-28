2ac2f7c1f26bcc257f4d79e31fdc99c1a9193e80
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-06-28 12:03:58 +0200

    latex symbol printing: greek sub- and superscripts

--- sympy/printing/tests/test_latex.py
+++ sympy/printing/tests/test_latex.py
@@ -182,4 +182,7 @@ def test_latex_issue1477():
     assert latex(Symbol("x_a_a^a^a")) == r"$x^{a,a}_{a,a}$"
     assert latex(Symbol("alpha_11")) == r"$\alpha_{11}$"
     assert latex(Symbol("alpha_11_11")) == r"$\alpha_{11,11}$"
+    assert latex(Symbol("alpha_alpha")) == r"$\alpha_{\alpha}$"
+    assert latex(Symbol("alpha^aleph")) == r"$\alpha^{\aleph}$"
+
 
